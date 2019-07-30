644ba6ed6b75c11e6c7b8f414140c696a407413b
Devender Dagar <Devender.Dagar@KMG.COM>
2019-07-30 11:20:13 +0530

    Custom Controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Everty time when using routing or first step for routing
+import { RouterModule, } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+
+const routes = [{path:'', component:HomeComponent},
+{path:'home', component:HomeComponent}];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
